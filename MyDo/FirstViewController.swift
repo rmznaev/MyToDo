7e2c5f3a0b09ce72994a4f0c25615afa398e4654
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-13 00:52:05 +0400

    Initial Commit

---
+++ MyDo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyDo
+//
+//  Created by Ramazan Abdullayev on 3/13/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyDo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyDo
+//
+//  Created by Ramazan Abdullayev on 3/13/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
