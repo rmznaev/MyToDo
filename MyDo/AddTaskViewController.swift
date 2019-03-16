4bf38a8ca1d82a157a1f057116e9605467b331b3
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-16 15:16:32 +0400

    Right/Left Swipe action added. All size app icons added for iPhone & iPad

--- MyDo/AddTaskViewController.swift
+++ MyDo/AddTaskViewController.swift
@@ -52,6 +52,30 @@ class AddTaskViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-
+    
 }
 
+class SwipableTabVCAddTask : AddTaskViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        left.direction = .left
+        self.view.addGestureRecognizer(left)
+        
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        right.direction = .right
+        self.view.addGestureRecognizer(right)
+    }
+    
+    @objc func swipeLeft() {
+        let total = self.tabBarController!.viewControllers!.count - 1
+        tabBarController!.selectedIndex = min(total, tabBarController!.selectedIndex + 1)
+        
+    }
+    
+    @objc func swipeRight() {
+        tabBarController!.selectedIndex = max(0, tabBarController!.selectedIndex - 1)
+    }
+}
--- MyDo/TasksViewController.swift
+++ MyDo/TasksViewController.swift
@@ -74,5 +74,29 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
  
     */
     
+}
+
+class SwipableTabVC : TasksViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        left.direction = .left
+        self.view.addGestureRecognizer(left)
+        
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        right.direction = .right
+        self.view.addGestureRecognizer(right)
+    }
+    
+    @objc func swipeLeft() {
+        let total = self.tabBarController!.viewControllers!.count - 1
+        tabBarController!.selectedIndex = min(total, tabBarController!.selectedIndex + 1)
+        
+    }
+    
+    @objc func swipeRight() {
+        tabBarController!.selectedIndex = max(0, tabBarController!.selectedIndex - 1)
+    }
 }
